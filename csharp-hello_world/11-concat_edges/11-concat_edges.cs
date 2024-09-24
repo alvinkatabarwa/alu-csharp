8bd24dca2d96fddf8d484e508432ff086183d40a
alvinkatabarwa <116846817+alvinkatabarwa@users.noreply.github.com>
2024-09-24 15:37:59 +0200

    Update 11-concat_edges.cs

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -2,9 +2,10 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
-        Console.WriteLine(str.Substring(48, 27) + " in " + str.Substring(78, 6));
-    }
+	static void Main(string[] args)
+	{
+		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
+		string a = str.Substring(49, 15); // "object-oriented"
+        string b = str.Substring(80, 11); // "programming" string c = str.Substring(101, 3); // "in C#"str = a + " " + b + " " + "in" + c ;Console.WriteLine(str);
+        }
 }
