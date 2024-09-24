9484673ecd1f6332814bc75a0d6eebdb6abe14d4
alvinkatabarwa <116846817+alvinkatabarwa@users.noreply.github.com>
2024-09-24 12:39:22 +0200

    q6

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        string str = "Holberton School";
+        Console.WriteLine(str + str + str);  // Print str three times
+        Console.WriteLine(str.Substring(0, 9)); // Print the first 9 characters
+
+    }
+}
