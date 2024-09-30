368dbe3caa8cdef537803354a5e46b88ec6b6278
alvinkatabarwa <116846817+alvinkatabarwa@users.noreply.github.com>
2024-09-30 06:37:12 +0200

    Update 0-print_array.cs

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,43 +1,16 @@
-﻿using System;
+using System;
 
-class Array
+class Program
 {
-    // create and print an array of integers based on the provided size
-    public static int?[] CreatePrint(int size)
+    static void Main(string[] args)
     {
-        // Check if size is less than 0, print error message and return null
-        if (size < 0)
+        int[] array = { 0, 1, 2, 3, 4 };
+
+        foreach (var item in array)
         {
-            Console.WriteLine("Size cannot be negative");
-            return null;
+            Console.Write(item + " ");
         }
-
-        // Create an array of integers with the given size
-        int?[] array = new int?[size];
-
-        // If size is 0, just return the array and print a blank line
-        if (size == 0)
-        {
-            Console.WriteLine();
-            return array;
-        }
-
-        // Fill the array with numbers starting from 0 and print them
-        for (int i = 0; i < size; i++)
-        {
-            array[i] = i;
-            Console.Write($"{i}");
-
-          
-            if (i < size - 1)
-            {
-                Console.Write(" ");
-            }
-        }
-
-      
         Console.WriteLine();
-        
-        return array;
+        Console.WriteLine("Array Length: " + array.Length);
     }
 }
