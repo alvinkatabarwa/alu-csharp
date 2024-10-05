2b218a9e09e55bfa5f56386bd46e852e107c1ba9
alvinkatabarwa <116846817+alvinkatabarwa@users.noreply.github.com>
2024-10-05 23:29:36 +0200

    Update 2-divide_lists.cs

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -1,12 +1,13 @@
 using System;
-using Systems.Collections.Generic;
+using System.Collections.Generic; // Correct namespace
 
-class List
+class ListOperations // Changed class name to avoid conflict with built-in List
 {
     public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
         List<int> result = new List<int>();
         int div = 0;
+
         for (int i = 0; i < listLength; i++)
         {
             try
@@ -18,16 +19,13 @@ class List
             {
                 Console.WriteLine("Out of range");
             }
-            catch (ArgumentOutOfRangeException)
-            {
-                Console.WriteLine("Out of range");
-            }
             catch (DivideByZeroException)
             {
                 Console.WriteLine("Cannot divide by zero");
-                result.Add(0);
+                result.Add(0); // Handle division by zero by adding 0 to result
             }
         }
+
         return result;
     }
 }
