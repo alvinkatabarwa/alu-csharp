f8a7b5c16458f192b9e4f13fa53892f84ffc9097
alvinkatabarwa <116846817+alvinkatabarwa@users.noreply.github.com>
2024-10-05 18:27:34 +0200

    question 0

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i;
+        int count = 0;
+        try
+        {
+            for (i = 0 ; i < n ; i++)
+            {
+                Console.WriteLine(myList[i]);
+                count++;
+            }
+        }
+        catch (Exception)
+        {
+            Console.Write("");
+        }
+        return(count);
+    }
+}
