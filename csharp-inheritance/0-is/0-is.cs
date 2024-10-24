407e53be880f2f80b503e26ea979d1dedc459949
alvinkatabarwa <116846817+alvinkatabarwa@users.noreply.github.com>
2024-10-24 15:58:06 +0200

    question 1

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,8 @@
+﻿public class Obj
+{
+    // this method if checks if the object is an int
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int; // this returns true if obj is of type int, otherwise false
+    }
+}
